6746ff3850261015ba947a11072a1701fd0b0b15
YazminPonce <yazmin.ponceh@gmail.com>
2023-05-30 00:27:48 -0600

    Vista empleado y materiales

---
+++ proyectoNetbeans/Proyecto_CP/web/js/main.js
@@ -0,0 +1,46 @@
+let moduloEmpleado;
+let moduloMaterial;
+
+
+function cargarModuloEmpleado(){
+fetch("modulos/moduloEmpleado/vista_empleado.html")
+        .then(
+                function(response){
+                return response.text();
+                }
+        )
+        .then(
+                function(html){
+                document.getElementById("contenedorPrincipal").innerHTML = html;
+                import ("../modulos/moduloEmpleado/controlador_empleado.js").then(
+                        function(controller){
+                        moduloEmpleado = controller;
+                               // moduloCliente.inicializar(); //Mandamos llamar la función inicializar
+                        }
+                );
+                }
+        );
+        }
+        
+function cargarModuloMaterial(){
+fetch("modulos/moduloMaterial/vista_material.html")
+        .then(
+                function(response){
+                return response.text();
+                }
+        )
+        .then(
+                function(html){
+                document.getElementById("contenedorPrincipal").innerHTML = html;
+                import ("../modulos/moduloMaterial/controlador_material.js").then(
+                        function(controller){
+                        moduloMaterial = controller;
+                              //  moduloMaterial.inicializar(); //Mandamos llamar la función inicializar
+                        }
+                );
+                }
+        );
+        }       
+
+
+
